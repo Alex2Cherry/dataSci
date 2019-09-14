01741b00c7109fce075041430f305333e1d59bf9
Alex2Cherry <alexliu1354@qq.com>
2019-09-14 22:42:46 +0800

    fakeData 1.0 without touchData

--- fakeData/fakeData.py
+++ fakeData/fakeData.py
@@ -2,7 +2,9 @@
 # -*- coding: UTF-8 -*-
 
 import json
+import random
 import time
+import copy
 from typing import List, Any
 
 
@@ -18,11 +20,45 @@ def getTouchData(amount = 64):
 
 def getAccData(amount = 300) -> object:
     accData: List[Any] = []
+
+    startTime = int(time.time() * 1000)
+    max_x = 4.490255453651858
+    min_x = -7.425179762145812
+    max_y = 3.0074126327164348
+    min_y = -5.769433205108815
+    max_z = 12.511322
+    min_z = 4.4683566
+
+    dataItem = dict()
+    for idx in range(amount):
+        dataItem["t"] = startTime + 200 * idx
+        dataItem["x"] = random.uniform(min_x, max_x)
+        dataItem["y"] = random.uniform(min_y, max_y)
+        dataItem["z"] = random.uniform(min_z, max_z)
+        accData.append(copy.deepcopy(dataItem))
+
     return accData
 
 
 def getGyroData(amount = 300):
     gyroData = []
+
+    startTime = int(time.time() * 1000)
+    max_x = 0.7631976
+    min_x = -0.9423207
+    max_y = 1.172355
+    min_y = -2.0713217
+    max_z = 0.5539326
+    min_z = -0.91334826
+
+    dataItem = dict()
+    for idx in range(amount):
+        dataItem["t"] = startTime + 200 * idx
+        dataItem["x"] = random.uniform(min_x, max_x)
+        dataItem["y"] = random.uniform(min_y, max_y)
+        dataItem["z"] = random.uniform(min_z, max_z)
+        gyroData.append(copy.deepcopy(dataItem))
+
     return gyroData
 
 
